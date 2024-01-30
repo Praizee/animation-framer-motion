4bbb736b3806c6a5d35602707276c72cd850a412
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-30 12:38:16 +0100

    assets

---
+++ components/images.js
@@ -0,0 +1,20 @@
+import Image1 from "./../assets/pexels-barry-tan-7995039.jpg";
+import Image2 from "./../assets/pexels-fayson-merege-5627427.jpg";
+import Image3 from "./../assets/pexels-guillaume-hankenne-2583494.jpg";
+import Image4 from "./../assets/pexels-josh-hild-13582363.jpg";
+import Image5 from "./../assets/pexels-marco-allasio-5653749.jpg";
+import Image6 from "./../assets/pexels-philipp-aleev-9323386.jpg";
+import Image7 from "./../assets/pexels-sultan-raimosan-6307029.jpg";
+import Image8 from "./../assets/pexels-vadimsadovski-7049988.jpg";
+
+// export default [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8];
+export default [Image1, Image2, Image3, Image4, Image5, Image7];
+
+export const destination = [
+  { title: "Lagos to Ibadan" },
+  { title: "Lagos to Edo" },
+  { title: "Lagos to PH" },
+  { title: "Lagos to Jos" },
+  { title: "Lagos to Abuja" },
+  { title: "Lagos to Ife" },
+];
