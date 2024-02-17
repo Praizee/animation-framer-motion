6cec346f8cd616592ce8f3581f7924c9c9a3f3f2
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-17 09:06:50 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,21 @@
 "use client";
 
 import CustomCarousel from "@/components/CustomCarousel";
+import Login from "@/components/Login";
+import Signup from "@/components/Signup";
 import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <section>
-      <motion.div animate={{ x: 250 }}>Hello World!</motion.div>
+    <section className="">
+      <Signup />
+
+      {/* <Login /> */}
+
+      {/* <motion.div animate={{ x: 250 }}>Hello World!</motion.div>
       <div>
         <CustomCarousel />
-      </div>
+      </div> */}
     </section>
   );
 }
